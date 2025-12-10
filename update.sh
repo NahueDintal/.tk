b6ac7b5e8db96bc13a9970d8d1b754774ff7c102
nahuedintal <nahue.dintal@gmail.com>
2025-12-09 22:06:55 -0300

    agreagado de script en bash para actualizar el .tar y así comprobar los cambios

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ javac --release 25 -d bin src/main/java/Main.java src/main/java/*.java
 
 if [ $? -eq 0 ]; then
   jar cfm tk.jar MANIFEST.MF -C bin .
-  echo "Tasker actualizado correctamente"
+  echo "Tasker actualizado correctamente!"
 else
   echo "Error en compilación"
 fi
